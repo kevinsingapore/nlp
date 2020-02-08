f3c1bf707b789199abc9c08986f527e616724b9c
kevin <948876386@qq.com>
2020-02-08 22:05:53 +0800

    init

---
+++ chapter-2/numpy/demo00.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#-*- coding:UTF-8 -*-
+
+import numpy as np
+
+vector = np.array([1,2,3,4])
+
+matrix = np.array([[1,'tim'],[2,'keivn'],[3,'jane'],[4,'john']])
+
+print(vector)
+
+print(matrix)
